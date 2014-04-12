1b62c25232c4d5dfa2b49aee49e7f918fafc5e2c
nicholas <nicholas@ubuntu.(none)>
2014-04-11 22:42:46 -0400

    added the other files I was supposed to commit before

--- app/assets/javascripts/scheduler.js
+++ app/assets/javascripts/scheduler.js
@@ -1,1 +1,16 @@
-//HAVE FUN GUYS
+$('#schedule').fullCalendar({
+    defaultView: 'agendaWeek',
+    weekends: false,
+    firstHour: 8,
+    allDaySlot: false,
+    columnFormat: {
+    	agendaWeek: 'dddd'
+    },
+    titleFormat: {
+    	agendaWeek: 'yyyy'
+    },
+    contentHeight: 610
+    //aspectRatio: 2
+});
+
+$('.fc-header-right').css('visibility', 'hidden');
